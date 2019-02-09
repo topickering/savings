882008d551a7cae7ba6e7ccb002c53a6b3cb9cfe
topickering <topickering@gmail.com>
2019-02-09 16:43:07 +0000

    Index page: tweak to welcome message

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -2,7 +2,7 @@ feature 'Index page' do
 
   scenario 'Index page test' do
     visit('/')
-    expect(page).to have_content("Hello World")
+    expect(page).to have_content("Plan for your future!")
   end
 
 end
