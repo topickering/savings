e46ece98becd150e4faf41cdf3c1c0c5c5576f61
topickering <topickering@gmail.com>
2019-02-09 16:13:22 +0000

    Pot Calculator: inflated target and current requirement functions added (NB compound maths assumed to be correct)

--- lib/pot_calculator.rb
+++ lib/pot_calculator.rb
@@ -1,7 +1,15 @@
 class PotCalculator
 
+  DEFAULT_INFLATION = 1.03
+  DEFAULT_INVESTMENT = 1.05
+
   def current_requirement(target, time)
-    
+    divisor = DEFAULT_INVESTMENT**time
+    inflated_target(target, time) / divisor
+  end
+
+  def inflated_target(target, time)
+    target*(DEFAULT_INFLATION**time)
   end
 
 end
--- spec/pot_calculator_spec.rb
+++ spec/pot_calculator_spec.rb
@@ -2,9 +2,17 @@ require 'pot_calculator'
 
 describe PotCalculator do
 
+  subject(:pc) { described_class.new }
+
   describe '#current_requirement' do
       it 'calculates the current amount needed to give a target pot with no additional contributions' do
-        expect(subject).to respond_to(:current_requirement).with(2)
+        expect(pc).to respond_to(:current_requirement).with(2)
+      end
+  end
+
+  describe '#inflated_target' do
+      it 'calculates an estimated target amount increased by inflation' do
+        expect(pc).to respond_to(:inflated_target).with(2)
       end
   end
 
